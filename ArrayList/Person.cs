8718c1de5976fba99ffc1aab3700f493eaad43f5
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-08 09:51:38 +0100

    Initial commit

---
+++ ArrayList/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ArrayList
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Names = new List<string>();
+            Names.Add("Joe");
+            Names.Add("Bob");
+            Names.Add("Jack");
+            Names.Add("Steve");
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+            var Numbers = new List<int> {0,1,2,3,4,5,6,7,8,9,10};
+            foreach (var number in Numbers)
+            {
+                if (number % 2 == 0) 
+                {
+                    Console.WriteLine(number);  
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
